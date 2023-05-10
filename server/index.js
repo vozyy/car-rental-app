9344e6fb22f885a60d0e1c15aecb96ee9112c289
vozar.david <vozar.david8@gmail.com>
2023-05-10 16:04:55 +0200

    chore: connect mongodb(mongoose) to express

--- server/index.js
+++ server/index.js
@@ -1,12 +1,25 @@
-import express from 'express';
 import dotenv from 'dotenv';
+import express from 'express';
+import mongoose from 'mongoose';
 
 dotenv.config();
 const PORT = process.env.PORT;
 
+mongoose.set('strictQuery', false);
+
 const app = express();
 app.use(express.json());
 
-app.listen(PORT, () => {
-    console.log(`My app is running on port: ${PORT}`);
-})
+const start = async() => {
+    try {
+        await mongoose.connect(process.env.DB_CONNECTION)
+        
+        app.listen(PORT, () => {
+            console.log(`My app is running on port: ${PORT}`);
+        })
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+start();
