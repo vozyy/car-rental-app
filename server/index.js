81ad2eca607a98d1ad7f60bff12ac8a3552d81ed
vozar.david <vozar.david8@gmail.com>
2023-05-11 13:04:39 +0200

    refactor: move mongoDB code to db/config

--- server/index.js
+++ server/index.js
@@ -1,18 +1,22 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import mongoose from 'mongoose';
 import { auth } from 'express-openid-connect';
 import config from './src/rest/middleware/config.js';
-import start from './src/db/config/config.js';
+import connectDB from './src/db/config/config.js';
 
 dotenv.config();
 
 const PORT = process.env.PORT;
 
-mongoose.set('strictQuery', false);
-
 const app = express();
 app.use(express.json());
 app.use(auth(config));
 
-start(app, PORT);
+const start = async () => {
+  await connectDB();
+  app.listen(PORT, () => {
+    console.log(`The app is running on port: ${PORT}`);
+  });
+};
+
+start();
