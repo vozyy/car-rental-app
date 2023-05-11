f4502e111e98aba6fa95a8b136900fae49285fe7
vozar.david <vozar.david8@gmail.com>
2023-05-11 13:21:41 +0200

    refactor: start backend server

--- server/index.js
+++ server/index.js
@@ -1,21 +1,14 @@
-import express from 'express';
 import dotenv from 'dotenv';
-import { auth } from 'express-openid-connect';
-import config from './src/rest/middleware/config.js';
 import connectDB from './src/db/config/config.js';
+import server from './src/index.js';
 
 dotenv.config();
-
 const PORT = process.env.PORT;
-
-const app = express();
-app.use(express.json());
-app.use(auth(config));
 
 const start = async () => {
   await connectDB();
-  app.listen(PORT, () => {
-    console.log(`The app is running on port: ${PORT}`);
+  server.listen(PORT, () => {
+    console.log(`The backend is running on port: ${PORT}`);
   });
 };
 
