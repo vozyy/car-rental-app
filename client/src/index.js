e425ee20ee0d87e3aae89f50f294afffa0ae2791
vozar.david <vozar.david8@gmail.com>
2023-05-13 16:25:58 +0200

    feat: implement react-router-dom

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
-import App from './App';
+import App from './features/App/App';
+import LoginForm from './components/Login/LoginForm';
 
+const router = createBrowserRouter([
+  { path: '/', element: <App /> },
+  {
+    path: '/login',
+    element: <LoginForm />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
