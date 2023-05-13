67e42879f7148af3aed926ff25b44880f4d8aeb6
vozar.david <vozar.david8@gmail.com>
2023-05-13 15:30:33 +0200

    remove: delete auth0 from backend

--- server/src/rest/controllers/baseController.js
+++ server/src/rest/controllers/baseController.js
@@ -1,5 +1,4 @@
 const get = (req, res) => {
-  console.log(req.oidc.isAuthenticated());
   res.send('This is the RentCars backend');
 };
 
--- server/src/rest/index.js
+++ server/src/rest/index.js
@@ -1,13 +1,10 @@
 import express from 'express';
-import { auth } from 'express-openid-connect';
-import config from './middleware/config.js';
 import http from 'http';
 import routes from './routes';
 
 const app = express();
 const httpServer = http.createServer(app);
 
-app.use(auth(config));
 app.use(express.json());
 app.use(routes);
 
