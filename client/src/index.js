9a317e2da527eacfdc5edb4d61f9c013cfeaf8bf
vozar.david <vozar.david8@gmail.com>
2023-05-14 16:24:41 +0200

    refactor: change naming

--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import App from './features/App/App';
-import LoginForm from './components/Login/LoginForm';
+import Login from './features/Login/Login';
 
 const router = createBrowserRouter([
   { path: '/', element: <App /> },
   {
     path: '/login',
-    element: <LoginForm />,
+    element: <Login />,
   },
 ]);
 
