9c383b6be2c9cd3e9d663237317e4872e50821ed
vozar.david <vozar.david8@gmail.com>
2023-05-14 16:25:34 +0200

    feat: create login form w input validation

--- client/src/components/Login/LoginForm.jsx
+++ client/src/components/Login/LoginForm.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react';
+import LoginFormInputs from './LoginFormInputs';
+import inputs from './LoginInputs';
+import userSchema from '../../services/formValidationService';
 
-function Login() {
+function LoginForm() {
+  const [values, setValues] = useState({
+    email: '',
+    password: '',
+  });
+
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await userSchema.validate(values, { abortEarly: false });
+      setErrorMessage(null);
+    } catch (err) {
+      setErrorMessage(err.inner[0].message);
+    }
+  };
+
+  const handleChange = async (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
   return (
-    <div>Login Page</div>
-  )
+    <div className='login-form'>
+      <form onSubmit={handleSubmit}>
+        {inputs.map((input, i) => (
+          <LoginFormInputs
+            key={i}
+            id={input.name}
+            value={values[input.name]}
+            {...input}
+            onChange={handleChange}
+            errorMessage={input.error}
+          />
+        ))}
+        {errorMessage && <p>{errorMessage}</p>}
+        <button>Log In</button>
+      </form>
+    </div>
+  );
 }
 
-export default Login
+export default LoginForm;
