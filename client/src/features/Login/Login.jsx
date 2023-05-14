518a46f98662f2de0f6a7bffc8802ed7cd77c0d0
vozar.david <vozar.david8@gmail.com>
2023-05-14 16:25:50 +0200

    feat: create login component

---
+++ client/src/features/Login/Login.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Login.css';
+import LoginForm from '../../components/Login/LoginForm';
+
+function Login() {
+  return <LoginForm />;
+}
+
+export default Login;
