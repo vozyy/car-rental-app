1e32bb4daed74326457ef95150c9aab72195e11d
vozar.david <vozar.david8@gmail.com>
2023-05-16 13:46:33 +0200

    feat: create inputs array for the register comp.

---
+++ client/src/components/Register/RegisterInputs.js
@@ -0,0 +1,22 @@
+const inputs = [
+  {
+    name: 'email',
+    type: 'email',
+    required: true,
+    label: 'Email',
+  },
+  {
+    name: 'password',
+    type: 'text',
+    required: true,
+    label: 'Password',
+  },
+  {
+    name: 'confirmPassword',
+    type: 'text',
+    required: true,
+    label: 'Confirm password',
+  },
+];
+
+export default inputs;
