e8a5292fedec7b084afa8afbad8d6c4c0c8fb732
vozar.david <vozar.david8@gmail.com>
2023-05-16 17:42:57 +0200

    chore: import user from services and send in res

--- server/src/rest/controllers/baseController.js
+++ server/src/rest/controllers/baseController.js
@@ -1,5 +1,10 @@
+import user from '../../services/userService';
+
+// Here I just import the user I created an instance of na send it in the resBody
+
 const get = (req, res) => {
-  res.send('This is the RentCars backend');
+  //res.send('This is the RentCars backend');
+  res.send(user);
 };
 
 export default {
