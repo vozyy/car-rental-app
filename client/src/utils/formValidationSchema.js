5c273de0faefb803532e61886979aad64fdb008b
vozar.david <vozar.david8@gmail.com>
2023-05-16 13:47:01 +0200

    feat: add register schema and change naming

--- client/src/utils/formValidationSchema.js
+++ client/src/utils/formValidationSchema.js
@@ -1,6 +1,6 @@
 import * as yup from 'yup';
 
-const userSchema = yup.object().shape({
+export const loginSchema = yup.object().shape({
   email: yup.string().email('invalid email').required('valid email required'),
   password: yup
     .string()
@@ -12,4 +12,18 @@ const userSchema = yup.object().shape({
     .required('password is required'),
 });
 
-export default userSchema;
+export const registerSchema = yup.object().shape({
+  email: yup.string().email('invalid email').required('valid email required'),
+  password: yup
+    .string()
+    .min(8)
+    .matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/, {
+      message:
+        'password must be at least 8 characters long and contain at least 1 number and 1 special character',
+    })
+    .required('password is required'),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref('password'), null], 'Passwords must match')
+    .required(),
+});
