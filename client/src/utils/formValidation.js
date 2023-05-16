4ce3fc20059fc1e4b6f0f98219b00bb56fd130c0
vozar.david <vozar.david8@gmail.com>
2023-05-16 13:46:44 +0200

    refactor: remove comments

--- client/src/utils/formValidation.js
+++ client/src/utils/formValidation.js
@@ -2,8 +2,6 @@ const validateLoginInput = async (schema, inputValues) => {
   try {
     await schema.validate(inputValues, { abortEarly: false });
     return null;
-    // setErrorMessage(null);
-    // setShowErrorMessage(false);
   } catch (err) {
     return err;
   }
