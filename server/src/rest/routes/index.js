a4e31eb5f1621baca23244d056b1062bb1cec1d5
vozar.david <vozar.david8@gmail.com>
2023-05-17 20:13:16 +0200

    feat: import and use userRouter

--- server/src/rest/routes/index.js
+++ server/src/rest/routes/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import baseRouter from './baseRouter';
+import userRouter from './userRouter';
 
 const router = express.Router();
 
 router.use(baseRouter);
+router.use('/', userRouter);
 
 export default router;
