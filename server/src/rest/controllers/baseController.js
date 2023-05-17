65c5e5e58b85b11426ae5809728605444e86cb51
vozar.david <vozar.david8@gmail.com>
2023-05-17 20:12:02 +0200

    refactor: send a information message

--- server/src/rest/controllers/baseController.js
+++ server/src/rest/controllers/baseController.js
@@ -1,10 +1,5 @@
-import user from '../../services/userService';
-
-// Here I just import the user I created an instance of na send it in the resBody
-
 const get = (req, res) => {
-  //res.send('This is the RentCars backend');
-  res.send(user);
+  res.send('This is the RentCars backend');
 };
 
 export default {
