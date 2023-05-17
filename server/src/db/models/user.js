127ae2bc820d3345a24476ac36ecea685c31f7a0
vozar.david <vozar.david8@gmail.com>
2023-05-17 20:11:14 +0200

    refactor: rm comments, update schema

--- server/src/db/models/user.js
+++ server/src/db/models/user.js
@@ -1,11 +1,8 @@
 import mongoose from 'mongoose';
 
-// Step1
-// here I create the User model --> how should a user object look like
-// NEXT --> userServices.js
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
   email: { type: String, required: true },
+  password: { type: String, required: true },
 });
 
 const User = mongoose.model('User', userSchema);
