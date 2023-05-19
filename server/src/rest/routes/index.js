8633bcc3b2656977abc6cafff5a01f98b0dffa1b
vozar.david <vozar.david8@gmail.com>
2023-05-19 14:39:26 +0200

    feat: import and use vehicleRouter

--- server/src/rest/routes/index.js
+++ server/src/rest/routes/index.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import baseRouter from './baseRouter';
 import userRouter from './userRouter';
+import vehicleRouter from './vehicleRouter';
 
 const router = express.Router();
 
 router.use(baseRouter);
-router.use('/api', userRouter);
+router.use('/api', userRouter, vehicleRouter);
 
 export default router;
