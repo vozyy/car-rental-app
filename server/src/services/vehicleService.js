360c71907fee271e5c665a21f5cbbbb2eabd0800
vozar.david <vozar.david8@gmail.com>
2023-05-21 16:39:12 +0200

    feat: create updateVehicleWithRenter function

--- server/src/services/vehicleService.js
+++ server/src/services/vehicleService.js
@@ -1,12 +1,25 @@
 import Vehicle from '../db/models/vehicle';
 
-const getAllVehicles = () => {
+const getAllVehicles = async () => {
   try {
-    const vehicles = Vehicle.find();
+    const vehicles = await Vehicle.find();
     return vehicles;
   } catch (error) {
     throw new Error('Failed to retrieve vehicles');
   }
 };
 
-export default getAllVehicles;
+const updateVehicleWithRenter = async (renterId, vehicleId) => {
+  try {
+    const updateVehicle = await Vehicle.findOneAndUpdate(
+      { _id: vehicleId },
+      { $set: { rented_by: renterId } },
+      { new: true }
+    );
+    return updateVehicle;
+  } catch (error) {
+    throw new Error(`Failed to update vehicle in the database`);
+  }
+};
+
+export { getAllVehicles, updateVehicleWithRenter };
