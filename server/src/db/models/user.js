40af26a9ed1a0874d64061a6e5108c8bad8ded98
vozar.david <vozar.david8@gmail.com>
2023-05-21 16:33:38 +0200

    chore: improve code readability

--- server/src/db/models/user.js
+++ server/src/db/models/user.js
@@ -1,8 +1,14 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, required: true },
-  password: { type: String, required: true },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
 const User = mongoose.model('user', userSchema);
