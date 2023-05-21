42432273c7160a03d310fb86f8b6879f6d9259e7
vozar.david <vozar.david8@gmail.com>
2023-05-21 16:43:21 +0200

    feat: create new rout

--- server/src/rest/routes/vehicleRouter.js
+++ server/src/rest/routes/vehicleRouter.js
@@ -1,8 +1,14 @@
 import express from 'express';
+import checkToken from '../middleware/auth';
 import { vehicleController } from '../controllers';
 
 const vehicleRouter = express.Router();
 
-vehicleRouter.get('/vehicles', vehicleController.getAll);
+vehicleRouter.get('/vehicles', checkToken, vehicleController.getAll);
+vehicleRouter.patch(
+  '/vehicles/addRenter',
+  checkToken,
+  vehicleController.updateVehicleRenter
+);
 
 export default vehicleRouter;
