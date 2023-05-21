404b32ecec5d466fcf2e8723a225eb0b249fa692
vozar.david <vozar.david8@gmail.com>
2023-05-21 16:33:57 +0200

    feat: improve readability, add the rented_by field

--- server/src/db/models/vehicle.js
+++ server/src/db/models/vehicle.js
@@ -1,14 +1,43 @@
 import mongoose from 'mongoose';
 
 const vehicleSchema = new mongoose.Schema({
-  type: { type: String, required: true },
-  manufacturer_name: { type: String, required: true },
-  model_name: { type: String, required: true },
-  year: { type: Number, required: true },
-  transmittion: { type: String, required: true },
-  engine_type: { type: String, required: true },
-  seats: { type: Number, required: true },
-  price: { type: Number, required: true },
+  type: {
+    type: String,
+    required: true,
+  },
+  manufacturer_name: {
+    type: String,
+    required: true,
+  },
+  model_name: {
+    type: String,
+    required: true,
+  },
+  year: {
+    type: Number,
+    required: true,
+  },
+  transmittion: {
+    type: String,
+    required: true,
+  },
+  engine_type: {
+    type: String,
+    required: true,
+  },
+  seats: {
+    type: Number,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  rented_by: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    default: null,
+  },
 });
 
 const Vehicle = mongoose.model('vehicle', vehicleSchema);
