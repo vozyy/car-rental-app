dfb78fd7bf2d8c9714fcf941b4b96854b2f5163d
vozar.david <vozar.david8@gmail.com>
2023-05-21 16:41:56 +0200

    feat: export and impornt addRenterToVehicle

--- server/src/services/index.js
+++ server/src/services/index.js
@@ -1,4 +1,9 @@
 import createUser from './userService';
-import getAllVehicles from './vehicleService';
+import { getAllVehicles } from './vehicleService';
+import { addRenterToVehicle } from './vehicleService';
 
-export default { createUser, getAllVehicles };
+export default {
+  createUser,
+  getAllVehicles,
+  addRenterToVehicle,
+};
