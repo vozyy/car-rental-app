f95d452a1608e91beacfa773afb3c0b3b38c42a2
vozar.david <vozar.david8@gmail.com>
2023-05-22 20:49:29 +0200

    refactor: import module.css, update class naming

--- client/src/components/Register/RegisterFormInputs.jsx
+++ client/src/components/Register/RegisterFormInputs.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import styles from './RegisterFormInput.module.css';
 
 function RegisterFormInputs(props) {
   const { label, id, errorMessage, onChange, ...inputProps } = props;
 
   return (
     <>
-      <div className='register-form-input'>
-        <label htmlFor={id}>{label}: </label>
-        <input {...inputProps} id={id} onChange={onChange} />
-        <p className='register-input-error'>{errorMessage}</p>
+      <div className={styles['register-input-container']}>
+        <label className={styles['register-form-label']} htmlFor={id}>
+          {label}{' '}
+        </label>
+        <input
+          className={styles['register-form-input']}
+          {...inputProps}
+          id={id}
+          onChange={onChange}
+        />
+        <p className={styles['register-input-error']}>{errorMessage}</p>
       </div>
     </>
   );
