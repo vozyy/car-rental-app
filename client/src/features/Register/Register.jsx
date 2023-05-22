620977e5911c068a9db508a5354db7be78afe89e
vozar.david <vozar.david8@gmail.com>
2023-05-22 20:49:58 +0200

    feat: add img and slogan

--- client/src/features/Register/Register.jsx
+++ client/src/features/Register/Register.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import RegisterForm from '../../components/Register/RegisterForm';
+import styles from './Register.module.css';
 
 function Register() {
-  return <RegisterForm />;
+  return (
+    <>
+      <div className={styles['register-page-img']}></div>
+      <div className={styles['register-page-slogan']}>
+        Discover. <br /> Roam. Return. <br />
+      </div>
+      <RegisterForm />
+    </>
+  );
 }
 
 export default Register;
