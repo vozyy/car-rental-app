907a6b4a85df0b12ecf92e1381e1590660ed5b5b
vozar.david <vozar.david8@gmail.com>
2023-05-24 12:59:35 +0200

    refactor: replace elements

--- client/src/features/Register/Register.jsx
+++ client/src/features/Register/Register.jsx
@@ -6,9 +6,12 @@ function Register() {
   return (
     <>
       <div className={styles['register-page-img']}></div>
-      <div className={styles['register-page-slogan']}>
-        Discover. <br /> Roam. Return. <br />
-      </div>
+      <h1 className={styles['register-page-header']}>
+        Discover. <br /> Roam. <br /> Return. <br />
+      </h1>
+      <h2 className={styles['register-page-subheader']}>
+        Rent the car of your dreams with home delivery
+      </h2>
       <RegisterForm />
     </>
   );
