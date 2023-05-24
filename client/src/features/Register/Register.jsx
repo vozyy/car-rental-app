39926dae7c2ebb1dc25889f9b4dcb888a0c49d8a
vozar.david <vozar.david8@gmail.com>
2023-05-24 14:25:51 +0200

    feat: add content

--- client/src/features/Register/Register.jsx
+++ client/src/features/Register/Register.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import RegisterForm from '../../components/Register/RegisterForm';
 import styles from './Register.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { icon } from '@fortawesome/fontawesome-svg-core/import.macro';
 
 function Register() {
   return (
@@ -9,10 +11,39 @@ function Register() {
       <h1 className={styles['register-page-header']}>
         Discover. <br /> Roam. <br /> Return. <br />
       </h1>
-      <h2 className={styles['register-page-subheader']}>
-        Rent the car of your dreams with home delivery
-      </h2>
-      <RegisterForm />
+      <div className={styles['register-main-content-container']}>
+        <div className={styles['content-box']}>
+          <span>
+            <FontAwesomeIcon icon={icon({ name: 'car' })} />
+          </span>
+          <p>
+            Enjoy convenience at your doorstep with our car rentals app—have
+            your chosen vehicle delivered right to your home!
+          </p>
+        </div>
+        <div className={styles['content-box']}>
+          <span>
+            <FontAwesomeIcon icon={icon({ name: 'house' })} />
+          </span>
+          <p>
+            Convenience continues even after your rental period ends. We pick up
+            the car hassle-free at your address!
+          </p>
+        </div>
+        <div className={styles['content-box']}>
+          <span>
+            <FontAwesomeIcon icon={icon({ name: 'car-burst' })} />
+          </span>
+          <p>
+            Drive with confidence knowing that every car in our fleet comes with
+            comprehensive insurance coverage, keeping you fully protected.
+          </p>
+        </div>
+      </div>
+      <div>
+        <RegisterForm />
+      </div>
+      <div className={styles['empty']}></div>
     </>
   );
 }
