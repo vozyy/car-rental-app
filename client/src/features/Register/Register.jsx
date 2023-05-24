b320db4d9b79e16cd96c2c3d69438285195fd4eb
vozar.david <vozar.david8@gmail.com>
2023-05-24 17:46:38 +0200

    refactor: import AsideContent component

--- client/src/features/Register/Register.jsx
+++ client/src/features/Register/Register.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import RegisterForm from '../../components/Register/RegisterForm';
 import styles from './Register.module.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { icon } from '@fortawesome/fontawesome-svg-core/import.macro';
+import AsideContent from '../../components/Register/AsideContent';
 
 function Register() {
   return (
@@ -12,33 +11,21 @@ function Register() {
         Discover. <br /> Roam. <br /> Return. <br />
       </h1>
       <section className={styles['register-main-content-container']}>
-        <div className={styles['content-box']}>
-          <span>
-            <FontAwesomeIcon icon={icon({ name: 'car' })} />
-          </span>
-          <aside>
-            Enjoy convenience at your doorstep with our car rentals app—have
-            your chosen vehicle delivered right to your home!
-          </aside>
-        </div>
-        <div className={styles['content-box']}>
-          <span>
-            <FontAwesomeIcon icon={icon({ name: 'house' })} />
-          </span>
-          <aside>
-            Convenience continues even after your rental period ends. We pick up
-            the car hassle-free at your address!
-          </aside>
-        </div>
-        <div className={styles['content-box']}>
-          <span>
-            <FontAwesomeIcon icon={icon({ name: 'car-burst' })} />
-          </span>
-          <aside>
-            Drive with confidence knowing that every car in our fleet comes with
-            comprehensive insurance coverage, keeping you fully protected.
-          </aside>
-        </div>
+        <AsideContent
+          iconName='car'
+          content='Enjoy convenience at your doorstep with our car rentals app—have
+            your chosen vehicle delivered right to your home!'
+        />
+        <AsideContent
+          iconName='house'
+          content='Enjoy convenience at your doorstep with our car rentals app—have
+            your chosen vehicle delivered right to your home!'
+        />
+        <AsideContent
+          iconName='car-burst'
+          content='Enjoy convenience at your doorstep with our car rentals app—have
+            your chosen vehicle delivered right to your home!'
+        />
       </section>
       <div>
         <RegisterForm />
