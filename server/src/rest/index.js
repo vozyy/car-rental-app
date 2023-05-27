5606ecbdecde67b7346621e403a3c0894c494cb3
vozar.david <vozar.david8@gmail.com>
2023-05-27 14:36:27 +0200

    feat: add cors

--- server/src/rest/index.js
+++ server/src/rest/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import http from 'http';
+import cors from 'cors';
 import routes from './routes';
 
 const app = express();
 const httpServer = http.createServer(app);
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 export default httpServer;
