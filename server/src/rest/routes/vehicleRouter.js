91b7d2359e5e5a7407440083e2f032784fea6ab2
vozar.david <vozar.david8@gmail.com>
2023-05-28 12:09:15 +0200

    refactor: remove checkToken middleware

--- server/src/rest/routes/vehicleRouter.js
+++ server/src/rest/routes/vehicleRouter.js
@@ -1,13 +1,11 @@
 import express from 'express';
-import checkToken from '../middleware/auth';
 import { vehicleController } from '../controllers';
 
 const vehicleRouter = express.Router();
 
-vehicleRouter.get('/vehicles', checkToken, vehicleController.getAll);
+vehicleRouter.get('/vehicles', vehicleController.getAll);
 vehicleRouter.patch(
   '/vehicles/addRenter',
-  checkToken,
   vehicleController.updateVehicleRenter
 );
 
