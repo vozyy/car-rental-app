6fb208eacf3b735bf54883acf584a10273cb4ea0
vozar.david <vozar.david8@gmail.com>
2023-05-28 20:02:52 +0200

    feat: create component for car information

---
+++ client/src/components/HomePage/CarCard.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import styles from './CarCard.module.css';
+
+function CarCard(props) {
+  const {
+    carManufacturer,
+    carModel,
+    carYear,
+    carTransmittion,
+    carSeats,
+    carPrice,
+    onClick,
+  } = props;
+
+  return (
+    <div className={styles['car-card-container']}>
+      <div className={styles['car-manufacturer-info']}>
+        <h1>{carManufacturer}</h1>
+        <h3>{carModel}</h3>
+        <h5>{carYear}</h5>
+      </div>
+      <div className={styles['car-other-info']}>
+        <p>{carSeats}</p>
+        <p>{carTransmittion}</p>
+        <p>{carPrice}</p>
+      </div>
+      <button onClick={onClick}>Rent</button>
+    </div>
+  );
+}
+
+export default CarCard;
