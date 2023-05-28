6481b7baaf7a190483c2caab0a1d2a1e54ca34fe
vozar.david <vozar.david8@gmail.com>
2023-05-28 20:00:59 +0200

    feat: install morgan

--- server/src/rest/index.js
+++ server/src/rest/index.js
@@ -3,13 +3,15 @@ import http from 'http';
 import cors from 'cors';
 import routes from './routes';
 import checkToken from './middleware/auth';
+import morgan from 'morgan';
 
 const app = express();
 const httpServer = http.createServer(app);
 
+app.use(morgan('dev'));
 app.use(express.json());
-app.use(checkToken);
 app.use(cors());
+app.use(checkToken);
 app.use(routes);
 
 export default httpServer;
