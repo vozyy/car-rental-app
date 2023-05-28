a12117c707beeba8f4afcced61c51fed81c66ba9
vozar.david <vozar.david8@gmail.com>
2023-05-28 20:01:59 +0200

    chore: upgrade error messages

--- server/src/rest/middleware/auth.js
+++ server/src/rest/middleware/auth.js
@@ -8,17 +8,17 @@ const checkToken = async (req, res, next) => {
   }
   try {
     if (!req.headers.authorization) {
-      return res.status(401).json({ error: 'Missing token' });
+      return res.status(401).json({ error: 'Missing token, please sign in' });
     }
     const token = req.headers.authorization.split(' ')[1];
     jwt.verify(token, process.env.TOKEN_SECRET);
     next();
   } catch (error) {
     if (error.name === 'TokenExpiredError') {
-      res.status(401).json({ error: 'Token expired' });
+      res.status(401).json({ error: 'Token expired, please sign in again' });
       return res.end();
     }
-    res.status(401).json({ error: 'Invalid token' });
+    res.status(401).json({ error: 'Invalid token. please sign in' });
     return next(error.message);
   }
 };
