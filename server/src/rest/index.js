17ee265ae12770368f57bbb31f4b1bdd85ed8f4a
vozar.david <vozar.david8@gmail.com>
2023-05-28 12:09:33 +0200

    refactor: add checkToken middleware

--- server/src/rest/index.js
+++ server/src/rest/index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import http from 'http';
 import cors from 'cors';
 import routes from './routes';
+import checkToken from './middleware/auth';
 
 const app = express();
 const httpServer = http.createServer(app);
 
 app.use(express.json());
+app.use(checkToken);
 app.use(cors());
 app.use(routes);
 
