9c81449846e627ce58c5e231aff589bcc0957981
vozar.david <vozar.david8@gmail.com>
2023-06-01 17:35:22 +0200

    feat: add buttons prop

--- client/src/components/SwalAlert.jsx
+++ client/src/components/SwalAlert.jsx
@@ -2,17 +2,18 @@ import swal from 'sweetalert';
 
 import { useEffect } from 'react';
 
-function SwalAlert({ title, text, icon }) {
+function SwalAlert({ title, text, icon, buttons }) {
   useEffect(() => {
     const swalAlert = async () => {
       await swal({
         title,
         text,
         icon,
+        buttons,
       });
     };
     swalAlert();
-  }, [title, text, icon]);
+  }, [title, text, icon, buttons]);
   return null;
 }
 
