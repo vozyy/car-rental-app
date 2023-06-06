3af883d44ff91862639d4d8666976621e33d3a6e
vozar.david <vozar.david8@gmail.com>
2023-06-06 18:03:06 +0200

    feat: create getDateDifference function

--- client/src/utils/dateManipulation.js
+++ client/src/utils/dateManipulation.js
@@ -3,3 +3,13 @@ import moment from 'moment';
 export const formatDate = (date) => {
   return moment(date).format('DD/MM/YYYY');
 };
+
+export const getDateDifference = (start, end) => {
+  const startDate = new Date(start);
+  const endDate = new Date(end);
+
+  const timeDifference = endDate.getTime() - startDate.getTime();
+  const differenceInDays = Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
+
+  return differenceInDays;
+};
