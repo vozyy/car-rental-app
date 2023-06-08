5b3e56b7942b4c8851bb0ee7efe452f00d9af4d8
vozar.david <vozar.david8@gmail.com>
2023-06-08 21:40:25 +0200

    feat: implement rentalRouter

--- server/src/rest/routes/index.js
+++ server/src/rest/routes/index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import baseRouter from './baseRouter';
 import userRouter from './userRouter';
 import vehicleRouter from './vehicleRouter';
+import rentalRouter from './rentalRouter';
 
 const router = express.Router();
 
 router.use(baseRouter);
 router.use('/api', userRouter);
 router.use('/api', vehicleRouter);
+router.use('/api', rentalRouter);
 
 export default router;
