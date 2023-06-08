16679e7a407ee97a0a565f0958c3aabbc4c3f381
vozar.david <vozar.david8@gmail.com>
2023-06-08 21:40:08 +0200

    refactor: remove unuse route

--- server/src/rest/routes/vehicleRouter.js
+++ server/src/rest/routes/vehicleRouter.js
@@ -4,9 +4,5 @@ import { vehicleController } from '../controllers';
 const vehicleRouter = express.Router();
 
 vehicleRouter.get('/vehicles', vehicleController.getAll);
-vehicleRouter.patch(
-  '/vehicles/addRenter',
-  vehicleController.updateVehicleRenter
-);
 
 export default vehicleRouter;
