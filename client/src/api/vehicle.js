041a26b33a5c12ade802a35f5ed3cafc6ebe9f2e
vozar.david <vozar.david8@gmail.com>
2023-06-11 15:10:17 +0200

    chore: move createRenbtal to rental.js

--- client/src/api/vehicle.js
+++ client/src/api/vehicle.js
@@ -22,20 +22,3 @@ export const getVehicles = async (
     setErrorMessage(error);
   }
 };
-
-export const createRental = async (token, fetchData) => {
-  const response = await fetch(
-    `${process.env.REACT_APP_BACKEND_URL}/api/create-rental`,
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        authorization: `Bearer ${token}`,
-      },
-      body: JSON.stringify(fetchData),
-    }
-  );
-
-  const resBody = await response.json();
-  return resBody;
-};
