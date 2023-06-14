f2d47b05c99fde35b23db09945e6c1456f1bd7e3
vozar.david <vozar.david8@gmail.com>
2023-06-14 14:57:01 +0200

    feat: import&implement svg icons

--- client/src/components/CarCard.jsx
+++ client/src/components/CarCard.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 import styles from './CarCard.module.css';
 import Calendar from './Calendar';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faPerson,
+  faGear,
+  faEuroSign,
+} from '@fortawesome/free-solid-svg-icons';
+
 function CarCard({
   carManufacturer,
   carModel,
@@ -13,20 +21,35 @@ function CarCard({
   selectedCarId,
   _id,
 }) {
+  library.add(faPerson, faGear, faEuroSign);
+
   return (
     <div className={styles['car-card-container']}>
       <div className={styles['car-manufacturer-info']}>
-        <h1>{carManufacturer}</h1>
+        <h3>{carManufacturer}</h3>
         <h3>{carModel}</h3>
         <h5>{carYear}</h5>
       </div>
+      {/* add a image property to the DB and render it here */}
+      <img src='' alt='here comes a car img' />
       <div className={styles['car-other-info']}>
-        <p>seats {carSeats}</p>
-        <p>transition {carTransmittion}</p>
-        <p>price {carPrice}€ / day</p>
+        <p>
+          <FontAwesomeIcon icon={faPerson} /> {carSeats}
+        </p>
+        <p>
+          <FontAwesomeIcon icon={faGear} /> {carTransmittion}
+        </p>
+        <p>
+          {' '}
+          <FontAwesomeIcon icon={faEuroSign} /> {carPrice}/day
+        </p>
       </div>
-      <button onClick={onClick}>Pick dates</button>
-      {selectedCarId === _id && <Calendar />}
+      <div className={styles['date-picker-button-container']}>
+        <button className={styles['dates-button']} onClick={onClick}>
+          Pick dates
+        </button>
+        {selectedCarId === _id && <Calendar />}
+      </div>
     </div>
   );
 }
