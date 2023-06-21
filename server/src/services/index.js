54e2836481e9d381dd13d3526b03d6d3d3fc93a2
vozar.david <vozar.david8@gmail.com>
2023-06-21 21:47:48 +0200

    chore: import&export new service

--- server/src/services/index.js
+++ server/src/services/index.js
@@ -1,10 +1,12 @@
 import createUser from './userService';
 import { getAllVehicles, getVehicleById } from './vehicleService';
 import postRentalData from './rentalService';
+import retrieveUsersRentals from './rentalService';
 
 export default {
   createUser,
   getAllVehicles,
   getVehicleById,
   postRentalData,
+  retrieveUsersRentals,
 };
