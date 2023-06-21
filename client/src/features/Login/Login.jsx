9c3b9d0cb190b7d4c1537691d06042da284b4941
vozar.david <vozar.david8@gmail.com>
2023-06-21 15:59:43 +0200

    feat: add header

--- client/src/features/Login/Login.jsx
+++ client/src/features/Login/Login.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
-import './Login.css';
+import styles from './Login.module.css';
 import LoginForm from './LoginForm';
 
 function Login() {
-  return <LoginForm />;
+  return (
+    <div className={styles['login-container']}>
+      <h1 className={styles['login-header']}>
+        Get back behind the steering wheel.
+      </h1>
+      <LoginForm />
+    </div>
+  );
 }
 
 export default Login;
