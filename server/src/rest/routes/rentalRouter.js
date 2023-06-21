80c206944bd9aecf0c6e12a25d6656b01330775a
vozar.david <vozar.david8@gmail.com>
2023-06-21 18:33:00 +0200

    feat: add new route

--- server/src/rest/routes/rentalRouter.js
+++ server/src/rest/routes/rentalRouter.js
@@ -4,5 +4,6 @@ import { rentalConroller } from '../controllers';
 const rentalRouter = express.Router();
 
 rentalRouter.post('/create-rental', rentalConroller.postRentalInformation);
+rentalRouter.get('/history', rentalConroller.getRentHistory);
 
 export default rentalRouter;
