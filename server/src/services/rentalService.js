764876e7dcce2a8cefbca2d88b16c55798fb9812
vozar.david <vozar.david8@gmail.com>
2023-06-21 21:47:36 +0200

    feat: add service for getting rental data from db

--- server/src/services/rentalService.js
+++ server/src/services/rentalService.js
@@ -1,4 +1,5 @@
 import RentalInformation from '../db/models/rentalInformation';
+import Vehicle from '../db/models/vehicle';
 
 const postRentalData = async (rentalData) => {
   try {
@@ -10,4 +11,22 @@ const postRentalData = async (rentalData) => {
   }
 };
 
-export default { postRentalData };
+const retrieveUsersRentals = async (userId) => {
+  try {
+    const usersRentals = await RentalInformation.find({
+      user_id: userId,
+    }).exec();
+    const detailedRentalInformation = await RentalInformation.populate(
+      usersRentals,
+      {
+        path: 'car_id',
+        model: Vehicle,
+      }
+    );
+    return detailedRentalInformation;
+  } catch (error) {
+    return { error: error.message };
+  }
+};
+
+export default { postRentalData, retrieveUsersRentals };
